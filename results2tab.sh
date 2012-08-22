802bec1e29cf97e3d8cbdf5863da678ccefb8d36
Andres Erbsen <andres.erbsen@gmail.com>
2012-08-22 21:35:38 +0300

    use expected result values if possible, for clustering

--- results2tab.sh
+++ results2tab.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 for file in $1; do
-	n=$( grep "Nodes:"            $file | cut -f2 )
-	m=$( grep "Neighbour degree:" $file | cut -f2 )
-	r=$( grep "Remote degree:"    $file | cut -f2 )
-	f=$( grep "Failure rate:"     $file | cut -f2 )
-	s=$( grep "Route Stretch"     $file | cut -f2 )
+	n=$( echo $file | grep -P -o '\d+(?=.tgf.txt)' )
+	m=$( echo $file | grep -P -o '(?<=_m)\d+' )
+	r=$( echo $file | grep -P -o '\d+(?=remotes)' )
+	f=$( grep "Failure rate:" $file | cut -f2 )
+	s=$( grep "Route Stretch" $file | cut -f2 )
 	echo -e "$n\t$m\t$r\t$f\t$s"
 done
